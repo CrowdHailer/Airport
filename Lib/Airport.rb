d0d5d35b4e7c4626fbc7cdb16d013a85719414ed
Peter Saxton <peterhsaxton@gmail.com>
2014-01-24 10:50:16 +0000

    Airport is initialized with a number of gates, added test for weather conditions

---
+++ Lib/Airport.rb
@@ -0,0 +1,8 @@
+class Airport 
+	def initialize(number_of_gates)
+		@gates = number_of_gates
+	end
+	
+	attr_reader :gates
+	
+end
--- Spec/airport_spec.rb
+++ Spec/airport_spec.rb
@@ -5,4 +5,10 @@ describe Airport do
 		airport = Airport.new 12
 		expect(airport.gates).to eq(12)
 	end
+
+	it 'should return the weather conditions as "Clear"' do
+		airport = Airport.new 10
+		expect(airport.weather_conditions).to eq("Clear")
+		
+	end
 end
