f22b31dcb9f571bc8acbb797fe1f05f569b09c05
Peter Saxton <peterhsaxton@gmail.com>
2014-01-24 10:24:20 +0000

    Testing a plane can land

--- Spec/plane_spec.rb
+++ Spec/plane_spec.rb
@@ -1,8 +1,15 @@
 require './Lib/plane'
 
 describe Plane do
+
+	let(:plane) { Plane.new }
+
 	it 'has a flying status when created' do
-		expect(Plane.new).to be_flying
+		expect(plane).to be_flying
+	end
+
+	it 'can land' do
+		expect(plane.land).not_to be_flying
 	end
 
 end
